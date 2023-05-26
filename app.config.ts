ab6ab5c9e79e817037a39caffc6aa142fd16bd96
goldmyr1994 <mauroconte.1994@gmail.com>
2023-05-26 09:33:31 +0300

    userInterfaceStyle automatic

--- app.config.ts
+++ app.config.ts
@@ -11,7 +11,7 @@ const config: ExpoConfig = {
   version: '1.0.0',
   orientation: 'portrait',
   icon: './assets/icon.png',
-  userInterfaceStyle: 'light',
+  userInterfaceStyle: 'automatic',
   splash: lightOnDarkSplash as Splash,
   assetBundlePatterns: ['**/*'],
   ios: {
